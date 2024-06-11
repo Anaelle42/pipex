646ab435751ba2845eb56bf1e8f01bd9c49a4e5c
ahenault <ahenault@student.42.fr>
2024-06-11 17:05:52 +0200

    modif utils

--- bonus/pipex_utils_bonus.c
+++ bonus/pipex_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ahenault <ahenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 20:52:22 by ahenault          #+#    #+#             */
-/*   Updated: 2024/06/10 22:29:15 by ahenault         ###   ########.fr       */
+/*   Updated: 2024/06/11 15:56:29 by ahenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ int	print_msg(char *s1, char *s2)
 	str = ft_strjoin(s1, s2);
 	ft_putstr_fd(str, 2);
 	ft_putstr_fd("\n", 2);
-	free(str);
+	if (str)
+		free(str);
 	return (0);
 }
 
